15c7453d3745bf67250cb9cdca7afe5e71cfa85b
rashdogg74 <pauloriordan74@gmail.com>
2022-01-14 09:48:10 +0000

    Remove v1 and add newly designed take user input function

--- run.py
+++ run.py
@@ -1,14 +1,6 @@
 # Import the gspread library to use with google sheets api
 import gspread
 from google.oauth2.service_account import Credentials
-# Imports and starts the colorama function for people accessing on windows
-# devices and autoreset means after every print statement
-# the default color of the text printed to the terminal goes back to white
-import colorama
-from colorama import Fore
-# colorama.init(autoreset=True) maybe I will want to put this on for after 
-# each round
-from typing import Optional
 import random 
 
 # Google api information
@@ -81,69 +73,15 @@ def get_word() -> str:
 
 get_word()
 
-def play() -> None:
-    def colored_alphabet(hits: set[str], misses: set[str]) -> str:
-        """ 
-        Returns a string of letters of alphabet, color coded by hit, 
-        miss or unknown
-        """
-        def color_and_char(ascii_code: int) -> str:
-            char = chr(ascii_code)
-            color = Fore.GREEN if char in hits \
-                else Fore.RED if char in misses \
-                else Fore.LIGHTBLACK_EX
-            return color + char
-        return ''.join(color_and_char(ascii_code) for ascii_code in 
-            range(ord('A'), ord('Z') + 1))
-
-    def get_valid_answer() -> str:
-        answer: str | None = None
-        while not answer:
-            prompt = colored_alphabet(hits, misses) + Fore.LIGHTWHITE_EX + \
-            ' --> ' + Fore.WHITE
-            response = input(prompt)
-            if len(response) != 5:
-                print(Fore.WHITE + f"Your answer must have 5 letters")
-            elif response not in values_list:
-                print(Fore.WHITE + 'Not in word list')
-            else:
-                answer = response
-        return answer
-
-    def show_output_pattern() -> None:
-        """ 
-        Zip function accepts iterable items and merges them 
-        into a single tuple. 
-        The resultant value is a zip object that stores pairs of iterables. 
-        You can pass lists, tuples, sets, or dictionaries 
-        through the zip() function.
-        """
-        # 1st letter from word and first from answer
-        for word_letter, answer_letter in zip(word, answer):
-            # is answer_letter in word if false returns an integer 0 
-            # which is used for reference just before i.e. misses
-            [misses, hits][answer_letter in word].add(answer_letter)
-            # If true it returns boolean 1 and that aligns with hits because 
-            # of zero indexing. Then .add to the corresponding set.
-            display_color, display_char = \
-                (Fore.GREEN, word_letter) if word_letter == answer_letter \
-                else (Fore.YELLOW, answer_letter) if answer_letter in word \
-                else (Fore.WHITE, '*')
-            print(display_color + display_char, end='')
-        print()
-
-    #maybe this needs to be outside after the get_word()
-    # word = get_word() 
-    # print(word)
-    #Optional denotes that it is some type [str] or None (before the user has inputted their guess)
-    answer: str | None = None
-    hits: set[str] = set()
-    misses: set[str] = set()
-
-    while answer != word:
-        answer = get_valid_answer()
-        show_output_pattern()
+def take_user_input():
+    user_input = input("Please enter a 5 letter word as your guess:\n")
+    while (user_input.isalpha() != True or len(user_input) != 5):
+        print("Incorrect format, please enter a 5 letter word as your guess")
+        user_input = input("Please enter a 5 letter word as your guess:\n")
+    while (user_input not in values_list):
+        print("Word is not in word list")
+        user_input = input("Please enter a 5 letter word as your guess:\n")
+    
+    return user_input
 
 
-
-# play()
